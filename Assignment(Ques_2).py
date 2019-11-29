50106ba2a98ddbb7e1b526f99efd0cb14399d80d
surajgupta77 <51394168+surajgupta77@users.noreply.github.com>
2019-11-29 19:26:33 +0530

    Assignment(Question's 2 Soln)

---
+++ Assignment(Ques_2).py
@@ -0,0 +1,21 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> try:
+    print(2+'33')
+except TypeError as e:
+   print('Error Handled',e)
+try:
+    h = "garg1"
+    h=float(h)
+except ValueError as e:
+    print("Error Handled",e)
+class Attributes():
+    pass
+try:
+    print(object.attribute)
+except ArithmeticError as e:
+    print('error handled',e)
+
+
+
+
